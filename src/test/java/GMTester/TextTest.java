f171b1afe23c3a36270e89cc53b144e828b49f45
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2023-08-31 17:57:14 +0200

    packages deleted

--- src/test/java/GMTester/TextTest.java
+++ src/test/java/GMTester/TextTest.java
@@ -2,11 +2,9 @@ package GMTester;
 
 import org.junit.jupiter.api.Test;
 
-public class TextTest {
+public class TextTest extends BaseTest {
 
     @Test
     void textTest(){
-
-
-    }
+        page.navigate("http://localhost:7080/notification_message_rendered");   }
 }
