4d413cd302af4d6492b801ba461b9d17f80c289c
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2023-08-31 18:28:57 +0200

    textTest methods added

--- src/test/java/GMTester/TextTest.java
+++ src/test/java/GMTester/TextTest.java
@@ -5,6 +5,19 @@ import org.junit.jupiter.api.Test;
 public class TextTest extends BaseTest {
 
     @Test
-    void textTest(){
-        page.navigate("http://localhost:7080/notification_message_rendered");   }
+    void textTest() {
+        page.navigate("http://localhost:7080/notification_message_rendered");
+
+        // 1. .innerHTML() - tags + text
+        //System.out.println(page.innerHTML("#content"));
+
+        // 2. .innerText() - sam tekst widoczny dla czlowieka
+        //System.out.println(page.innerText("#content"));
+
+        // 3. textContent() - sam text, widoczny i niewidoczny (np: <span display:none>hidden)
+        //System.out.println(page.textContent("#content"));
+
+        // 4. getAttribute() - atrybuty elementów html
+        System.out.println(page.getAttribute("div img", "alt"));
+    }
 }
