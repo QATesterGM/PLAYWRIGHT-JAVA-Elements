89d82b2c1eb8c936fda48791daa5f2373194393a
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2023-09-14 20:02:23 +0200

    dialog excercises added

---
+++ src/test/java/GMTester/_12ShadowDOMTest.java
@@ -0,0 +1,18 @@
+package GMTester;
+
+import org.junit.jupiter.api.Test;
+
+public class _12ShadowDOMTest extends BaseTest {
+
+    @Test
+    void shadowDOMTest() {
+        page.navigate("http://localhost:7080/shadowdom");
+        System.out.println(page.locator("span[slot=my-text]").innerText());
+    }
+
+    @Test
+    void shadowDOMTest1() {
+        page.navigate("https://bugs.chromium.org/p/chromium/issues/list");
+        page.selectOption("#can", "New issues");
+    }
+}
