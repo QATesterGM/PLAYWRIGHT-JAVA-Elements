9cd5d72582cf78c7bcc506f6df63421ff77c0076
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2023-09-15 16:28:43 +0200

    download File excercises added

--- src/test/java/GMTester/_15UploadFileTest.java
+++ src/test/java/GMTester/_15UploadFileTest.java
@@ -3,7 +3,10 @@ package GMTester;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.*;
 
 public class _15UploadFileTest extends BaseTest {
 
@@ -12,6 +15,19 @@ public class _15UploadFileTest extends BaseTest {
         page.navigate("http://localhost:7080/upload");
         page.setInputFiles("#file-upload", Paths.get("screenshots/Premier.jpg"));
         page.locator("#file-submit").click();
-        PlaywrightAssertions.assertThat(page.locator("#uploaded-files")).hasText("Premier.jpg");
+        assertThat(page.locator("#uploaded-files")).hasText("Premier.jpg");
+    }
+
+    @Test
+    void uploadMultipleFilesTest() {
+        page.navigate("https://davidwalsh.name/demo/multiple-file-upload.php");
+        page.setInputFiles("#filesToUpload", new Path[]{
+                Paths.get("screenshots/Premier.jpg"),
+                Paths.get("screenshots/firefox.png")
+        });
+
+        page.waitForTimeout(3000);
+        PlaywrightAssertions.assertThat(page.locator("#fileList")).hasText("Premier.jpg" + "firefox.png");
+
     }
 }
