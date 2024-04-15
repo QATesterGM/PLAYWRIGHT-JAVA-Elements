2b29cda0196b6eb920711e8b88027d67c0c8fb78
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2024-04-15 12:31:56 +0200

    Backend tests added

--- src/test/java/Backend/RequestResponseTest.java
+++ src/test/java/Backend/RequestResponseTest.java
@@ -1,4 +1,16 @@
 package Backend;
 
-public class RequestResponseTest {
+import GMTesterPlaywrightActionsAndElements.BaseTest;
+import org.junit.jupiter.api.Test;
+
+class RequestResponseTest extends BaseTest {
+
+    @Test
+    void should_return_status_code_200_ok_test(){}
+
+    @Test
+    void should_return_request_method_get_test(){}
+
+    @Test
+    void http_handlers_test(){}
 }
